605e650126590018805a8b53c7e8d6ce7de844ce
Sergi Calvet <scalvet@profile.es>
2020-06-29 22:41:21 +0200

    S3Resource improvements

--- src/test/kotlin/com/github/scalvet/S3Resource.kt
+++ src/test/kotlin/com/github/scalvet/S3Resource.kt
@@ -18,25 +18,23 @@ import javax.inject.Inject
 
 class S3Resource :QuarkusTestResourceLifecycleManager{
 
-    private val BUCKET_NAME = "quarkus.test.bucket"
+    val bucketName = "quarkus.test.bucket"
 
     @Inject
     @field: Default
     lateinit var s3: LocalStackContainer
-
-    private var client: S3Client? = null
 
     override fun start(): kotlin.collections.Map<String, String>? {
         DockerClientFactory.instance().client()
         try {
             s3 = LocalStackContainer().withServices(Service.S3)
             s3.start()
-            client = S3Client.builder()
+            var client = S3Client.builder()
                     .endpointOverride(URI(endpoint()))
                     .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("accesskey", "secretKey")))
                     .httpClientBuilder(UrlConnectionHttpClient.builder())
                     .region(Region.US_EAST_1).build()
-            client.createBucket(Consumer { b: CreateBucketRequest.Builder -> b.bucket(BUCKET_NAME) })
+            client.createBucket(Consumer { b: CreateBucketRequest.Builder -> b.bucket(bucketName) })
         } catch (e: Exception) {
             throw RuntimeException("Could not start S3 localstack server", e)
         }
@@ -46,7 +44,7 @@ class S3Resource :QuarkusTestResourceLifecycleManager{
         properties["quarkus.s3.aws.credentials.type"] = "static"
         properties["quarkus.s3.aws.credentials.static-provider.access-key-id"] = "accessKey"
         properties["quarkus.s3.aws.credentials.static-provider.secret-access-key"] = "secretKey"
-        properties["bucket.name"] = BUCKET_NAME
+        properties["bucket.name"] = bucketName
         return properties
     }
 
@@ -57,6 +55,6 @@ class S3Resource :QuarkusTestResourceLifecycleManager{
     }
 
     private fun endpoint(): String {
-        return java.lang.String.format("http://%s:%s", s3.getContainerIpAddress(), s3.getMappedPort(Service.S3.getPort()))
+        return java.lang.String.format("http://%s:%s", s3.containerIpAddress, s3.getMappedPort(Service.S3.port))
     }
 }
